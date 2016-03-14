7d24ea6f8e6a25bc19ef8a458119bcbced0dfccc
wufucious <wonderfulwyq@163.com>
2016-03-14 20:23:50 +0100

    add link-layer socket

--- src/ccn-lite-contiki.h
+++ src/ccn-lite-contiki.h
@@ -132,6 +132,19 @@ struct sockaddr_in6 {
 };
 
 /**
+ * @brief Link-Layer socket descriptor
+ */
+struct sockaddr_ll {
+    unsigned short sll_family;   /**< Always AF_PACKET */
+    unsigned short sll_protocol; /**< Physical-layer protocol */
+    int            sll_ifindex;  /**< Interface number */
+    unsigned short sll_hatype;   /**< ARP hardware type */
+    unsigned char  sll_pkttype;  /**< Packet type */
+    unsigned char  sll_halen;    /**< Length of address */
+    unsigned char  sll_addr[8];  /**< Physical-layer address */
+};
+
+/**
  * @brief   IPv6 multicast request.
  */
 struct ipv6_mreq {
